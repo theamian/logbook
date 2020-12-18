0b41f1ecb56cacd82317d7a1d60cc6bd9075a003
theamian <damijan.nejasmic@gmail.com>
2020-12-18 19:43:52 +0100

    Add click on map to prefill the form

--- logbook/models.py
+++ logbook/models.py
@@ -2,8 +2,6 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.forms import ModelForm
 from django import forms
-from django_countries.fields import CountryField
-from django_countries.widgets import CountrySelectWidget
 
 class User(AbstractUser):
     pass
@@ -12,7 +10,7 @@ class LogEntry(models.Model):
     diver = models.ForeignKey(User, on_delete=models.CASCADE, related_name="dove")
     date = models.DateField(auto_now=False, auto_now_add=False)
     town = models.CharField(max_length=200)
-    country = CountryField(countries_flag_url='#')
+    country = models.CharField(max_length=200)
     lat = models.DecimalField(max_digits=16, decimal_places=14)
     lng = models.DecimalField(max_digits=17, decimal_places=14)
     buddy = models.CharField(max_length=100, blank=True)
@@ -25,9 +23,13 @@ class LogForm(ModelForm):
     date = forms.DateField(input_formats=["%d/%m/%Y"],
                             widget=forms.DateInput(attrs={"placeholder" : "dd/mm/yyyy"}
                             ))
-
     class Meta:
         model = LogEntry
-        fields = ("date", "town", "country", "buddy")
-        widgets = {'country': CountrySelectWidget()}
+        fields = ("date", "town", "country", "buddy", "lat", "lng")
+        widgets = {
+            "lat": forms.HiddenInput(),
+            "lng": forms.HiddenInput(),
+            "town": forms.TextInput(attrs={'readonly':'readonly'}),
+            "country": forms.TextInput(attrs={'readonly':'readonly'})
+        }
 
