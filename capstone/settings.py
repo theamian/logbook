c70589858eb19d759ce7a2cae56d33520d2eba23
theamian <damijan.nejasmic@gmail.com>
2021-01-21 22:28:11 +0100

    settings

--- capstone/settings.py
+++ capstone/settings.py
@@ -30,7 +30,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
 ALLOWED_HOSTS = []
 
